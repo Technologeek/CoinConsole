63759312fecfc400a1882f75016222f5de65e1ce
Michael Warner <michael@lumino.co>
2017-03-28 21:16:52 -0500

    GET request and filter
    Finished the client-side GET request functionality and added some
    styles for the filter checkboxes that it inserts. Also removed
    index.html and moved pug into it’s own views folder.

--- assets/js/cmcgod.js
+++ assets/js/cmcgod.js
@@ -1,20 +1,63 @@
 let charts = document.getElementById('charts'),
     filter = document.getElementById('filter');
 
-var data;
+var data,
+    toggled = false;
 
 //on page load, get all of the coin tickers (with ajax)
-/*window.onready({
-  get('/get');
-});*/
+window.onload = refresh();
 
-//set up a get function
+function refresh(){
+  console.log("refreshing data...");
+  get('/get', function(coins){
+    filter.innerHTML = null;
+    for (coin in coins){
+      var name = coins[coin].name;
+      //console.log(coins[coin]);
+      filter.innerHTML = filter.innerHTML + createCheckbox(name);
+    }
+  });
+}
+
+function toggleFilter(){
+  if (toggled == false) {
+    //show the filter
+    filter.style.display = "block";
+    toggled = true;
+  } else if (toggled == true) {
+    //hide the filter
+    filter.style.display = "none";
+    toggled = false;
+  }
+}
+
+//refresh every 10 seconds
+//setInterval(refresh(), 10000);
+
+//make those filter boxes
+function createCheckbox(s){
+  var labelHTML = "<label for=" + s + ">" + s + "</label>",
+      checkboxHTML = '<input type="checkbox" name="' + s + '">',
+      result = labelHTML + checkboxHTML;
+
+  return result;
+}
+
+//set up a GET function
 function get(url, callback){
+  if (! url) {
+    console.log('No url was passed in...');
+    return;
+  }
+
   xhr = new XMLHttpRequest();
 
   xhr.addEventListener("readystatechange", function () {
     if (this.readyState === 4) {
-      console.log(this.responseText);
+      data = JSON.parse(this.responseText);
+      console.log('Got the data...');
+      //console.log(this.responseText);
+      callback(data);
     }
   });
 
